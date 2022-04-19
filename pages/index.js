803b7808d95070ab9572fcfa4b07a5c492f9a69a
kimdaeyoon <eodbszla@gmail.com>
2022-04-19 09:51:54 +0900

    Add: Dynamic Router, Movie Detail pages

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,51 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
 import Seo from "../components/Seo";
 
 export default function Home({ results }) {
+  const router = useRouter();
+  const onClick = (id, title) => {
+    //router hook으로 페이지 이동뿐만 아니라 쿼리로 내용을 전달해줄 수 있다.
+    //push 2번째 인자에 유저에게 보일 url의 모습을 정의해주면 데이터를 쿼리에 전달해주면서 숨길수 있다
+    router.push(
+      {
+        pathname: `/movies/${id}`,
+        query: {
+          title,
+        },
+      },
+      `/movies/${id}`
+    );
+  };
   return (
     <div className="container">
       <Seo title="Home" />
       {results?.map((movie) => (
-        <div className="movie" key={movie.id}>
-          <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
-          <h4>{movie.original_title}</h4>
+        <div
+          onClick={() => onClick(movie.id, movie.original_title)}
+          className="movie"
+          key={movie.id}
+        >
+          <img
+            src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+            alt="Poster Image"
+          />
+          <h4>
+            {/* Link도 Router hook와 같이 기능한다 */}
+            <Link
+              href={
+                ({
+                  pathname: `/movies/${movie.id}`,
+                  query: {
+                    title: movie.original_title,
+                  },
+                },
+                `/movies/${movie.id}`)
+              }
+            >
+              <a>{movie.original_title} </a>
+            </Link>
+          </h4>
         </div>
       ))}
       <style jsx>{`
